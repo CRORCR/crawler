cfdce721e92616ae1ec87e3d1b9eb8e4e9eff24e
nihaiting <1350082993@qq.com>
2018-10-18 19:26:15 +0800

    1.获得页面信息 2.城市列表格式化 使用正则获得url和cityName

---
+++ regex/regexp.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+const text  = "my email address xingzhiheyi_code@163.com"
+func main() {
+	//re, _ := regexp.Compile("email")
+	//匹配邮箱 .:任意字符  +:0个或多个
+	re, _ := regexp.Compile(`[a-zA-Z_0-9]+@.+\..+`)
+	//匹配
+	match := re.FindAllStringSubmatch(text, -1)
+	fmt.Println(match)
+
+	//子匹配,所有匹配项分割开(把需要匹配的子项用()包裹)
+	re = regexp.MustCompile(`([a-zA-Z_0-9]+)@([a-zA-Z_0-9.]+)\.([a-zA-Z_0-9]+)`)
+	 submatch := re.FindAllStringSubmatch(text,-1)
+	 fmt.Println(submatch)
+
+}
